b153f02ec7e426ee67331de0d2636ab16f54c9f2
crowdbotics-challenges <andre@crowdbotics.com>
2019-08-23 19:01:35 +0000

    Updated tests for assessment

--- test.py
+++ test.py
@@ -1,26 +1,40 @@
 import unittest
 import re
 
+
 class TestStripe(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestStripe, self).__init__(*args, **kwargs)
         with open('order.html', 'r') as file_descriptor:
             self.dom_str = file_descriptor.read()
 
-    #Check if redirectToCheckout function call is present        
+    # Check if redirectToCheckout function call is present
     def test_redirect_to_checkout(self):
-        return True
-        #self.assertNotEqual(self.dom_str, '.redirectToCheckout', 'No stripe redirect call found!')
-    
-    #Check if successUrl redirects to order_success.html
+        self.assertNotEqual(self.dom_str, '.redirectToCheckout',
+                            'No stripe redirect call found!')
+
+    # Check if successUrl redirects to order_success.html
     def test_success_url(self):
-        return True
-        #self.assertRegex(self.dom_str, r'successUrl: \'https:\/\/[a-z]*\.com/order_success\.html\'', 'No order_success.html redirect found on checkout success.')
-    
-    #Check if cancelUrl redirects to order.html
+        self.assertRegex(self.dom_str,
+                         r'successUrl: \'https:\/\/[a-z]*\.com/order_success\.html\'',
+                         'No order_success.html redirect found on checkout success.')
+
+    # Check if cancelUrl redirects to order.html
     def test_cancel_url(self):
-        return True
-        #self.assertRegex(self.dom_str, r'cancelUrl: \'https:\/\/[a-z]*\.com/order\.html\'', 'No order.html redirect found on checkout cancel.')
+        self.assertRegex(self.dom_str,
+                         r'cancelUrl: \'https:\/\/[a-z]*\.com/order\.html\'',
+                         'No order.html redirect found on checkout cancel.')
+
+
+ 
+
+class TestSubmission(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestSubmission, self).__init__(*args, **kwargs)
+        with open('order.html', 'r') as file_descriptor:
+            self.dom_str = file_descriptor.read()
+        
+    
         
 if __name__ == '__main__':
     unittest.main()
